c057da390fb54b3ffa5328c02a528c0271d6a3d9
Jack Mair <jwjmair@gmail.com>
2017-10-17 20:41:29 +1300

    added an aggro range to enemy01s. streamlined ai code

--- Robotron/Robotron/Enemy01ControlSystem.cpp
+++ Robotron/Robotron/Enemy01ControlSystem.cpp
@@ -32,47 +32,55 @@ Enemy01ControlSystem::Enemy01ControlSystem(Scene& scene)
 {
 }
 
-//seek the enemy towards the player object every frame
+//the ai movement behavious for enemy01s
 void Enemy01ControlSystem::update(Entity& entity)
 {
 	if ((entity.componentMask & COMPONENT_ENEMY01) != COMPONENT_ENEMY01)
 		return;
 		
-	//find the closest player and set the target to positoin to their current position.
-	glm::vec3 targetPosition;
+	glm::vec4 targetPosition = { 100, 100, 100, 1 };
+	glm::vec4 pos = entity.transform[3];
+	float moveSpeed = entity.controlVars.moveSpeed;
+	bool isSeekingPlayer = false;
 
+	//find the closest player object to move to
 	for (unsigned int i = 0; i < m_scene.entities.size(); ++i)
 	{
-		if ((m_scene.entities.at(i).componentMask & COMPONENT_PLAYER_CONTROL) == COMPONENT_PLAYER_CONTROL)
+		if ((m_scene.entities.at(i).componentMask & COMPONENT_PLAYER_CONTROL) == COMPONENT_PLAYER_CONTROL //its a player object
+		  && glm::length(m_scene.entities.at(i).transform[3] - pos) < glm::length(targetPosition - pos)   //it is the closet player to the target
+		  && glm::length(m_scene.entities.at(i).transform[3] - pos) < 15)								  //the player is within the enemys aggro range
 		{
 			targetPosition = m_scene.entities.at(i).transform[3];
+			isSeekingPlayer = true;
 		}
 	}
 
-	glm::vec3 pos = entity.transform[3];
-	float moveSpeed = entity.controlVars.moveSpeed;
+	//TODO: ADD WANDERING CODE HERE
+	//if (!isSeekingPlayer)
+	//
+	if (isSeekingPlayer == true)
+	{
+		//determine the desired veloctiy to reach the target
+		float posMagnitude = glm::length(targetPosition - pos);
 
-	//determine the desired veloctiy to reach the target
-	float posMagnitude = glm::length(targetPosition);
-	
-	glm::vec3 desiredVelocity;
-	if (posMagnitude < 5)
-		desiredVelocity = (targetPosition - pos)/ posMagnitude * moveSpeed * (posMagnitude / 5); //apply arrival behaviour as the entity gets close to the target
-	else
-		desiredVelocity = (targetPosition - pos)/ posMagnitude * moveSpeed;
+		glm::vec3 desiredVelocity;
+		if (posMagnitude < 2)
+			desiredVelocity = (targetPosition - pos) / posMagnitude * moveSpeed * (posMagnitude / 2); //apply arrival behaviour as the entity gets close to the target
+		else
+			desiredVelocity = (targetPosition - pos) / posMagnitude * moveSpeed;
 
-	//determine the steering vector from the current veloctiy to the desired velocity
-	glm::vec3 steering = desiredVelocity - entity.physics.velocity;
-	
-	//further imrpovements:
-	//truncate steering vector by a maximum amount
-	//divide steering vector by object mass
+		//determine the steering vector from the current veloctiy to the desired velocity
+		glm::vec3 steering = desiredVelocity - entity.physics.velocity;
 
-	//determine the new velocty
-	glm::vec3 newVelocity;
-	newVelocity = entity.physics.velocity + steering;
+		//determine the new velocty
+		glm::vec3 newVelocity;
+		newVelocity = entity.physics.velocity + steering;
 
-	entity.transform[3] = { pos.x + newVelocity.x, pos.y + newVelocity.y, pos.z + newVelocity.z, 1}; //update the ojbects position with the new velocity
+		//TODO: ADD FLOCKING CODE HERE
+		//if (isSeekingPlayer)
+		//
 
+		entity.transform[3] = { pos.x + newVelocity.x, pos.y, pos.z + newVelocity.z, 1 }; //update the ojbects position with the new velocity
+	}
 	return;
 }
